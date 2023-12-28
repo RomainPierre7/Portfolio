2e0152a5432099005c31bac731cfe6d79ecbaea0
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-12-28 17:58:42 +0100

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://romainpierre7.github.io',
-  integrations: [react()]
+  site: 'https://romainpierre7.github.io/Portfolio/',
 });
